6b8895fdad208c6e17a7112e83f103dbc4c39a4a
Comma <84639718+Command17@users.noreply.github.com>
2026-07-01 19:13:34 +0200

    Revert my simplify changes

--- src/main/java/com/github/command17/neohammers/common/item/ModItems.java
+++ src/main/java/com/github/command17/neohammers/common/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
@@ -14,14 +15,14 @@ import java.util.function.Supplier;
 public final class ModItems {
     private static final DeferredRegister.Items REGISTER = DeferredRegister.createItems(NeoHammers.MOD_ID);
 
-    public static final Supplier<Item> WOODEN_HAMMER = createHammer("wooden_hammer", Tiers.WOOD, 5, -3.1f, of());
-    public static final Supplier<Item> STONE_HAMMER = createHammer("stone_hammer", Tiers.STONE, 6, -3.1f, of());
-    public static final Supplier<Item> IRON_HAMMER = createHammer("iron_hammer", Tiers.IRON, 5, -3.1f, of());
-    public static final Supplier<Item> GOLDEN_HAMMER = createHammer("golden_hammer", Tiers.GOLD, 5, -3.1f, of());
-    public static final Supplier<Item> DIAMOND_HAMMER = createHammer("diamond_hammer", Tiers.DIAMOND, 4, -3.1f, of());
-    public static final Supplier<Item> NETHERITE_HAMMER = createHammer("netherite_hammer", Tiers.NETHERITE, 4, -3, of().fireResistant());
+    public static final DeferredItem<HammerItem> WOODEN_HAMMER = createHammer("wooden_hammer", Tiers.WOOD, 5, -3.1f, of());
+    public static final DeferredItem<HammerItem> STONE_HAMMER = createHammer("stone_hammer", Tiers.STONE, 6, -3.1f, of());
+    public static final DeferredItem<HammerItem> IRON_HAMMER = createHammer("iron_hammer", Tiers.IRON, 5, -3.1f, of());
+    public static final DeferredItem<HammerItem> GOLDEN_HAMMER = createHammer("golden_hammer", Tiers.GOLD, 5, -3.1f, of());
+    public static final DeferredItem<HammerItem> DIAMOND_HAMMER = createHammer("diamond_hammer", Tiers.DIAMOND, 4, -3.1f, of());
+    public static final DeferredItem<HammerItem> NETHERITE_HAMMER = createHammer("netherite_hammer", Tiers.NETHERITE, 4, -3, of().fireResistant());
 
-    private static Supplier<Item> createHammer(String id, Tier tier, float attackDamage, float attackSpeed, Item.Properties properties) {
+    private static DeferredItem<HammerItem> createHammer(String id, Tier tier, float attackDamage, float attackSpeed, Item.Properties properties) {
         return REGISTER.registerItem(id,
                 (props) -> new HammerItem(tier, ModTags.BlockTags.MINEABLE_WITH_HAMMER, 2, props),
                 properties.attributes(DiggerItem.createAttributes(tier, attackDamage, attackSpeed)));
